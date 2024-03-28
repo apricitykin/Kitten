44034ed618969da7f9b7c1b4191e2cd070d869ee
Akira <104577281+apricitykin@users.noreply.github.com>
2024-03-28 17:44:29 +0100

    Update Vencord.ts

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -94,7 +94,7 @@ async function init() {
                 await update();
                 if (Settings.autoUpdateNotification)
                     setTimeout(() => showNotification({
-                        title: "Vencord has been updated!",
+                        title: "KITTYCORD UPDATED YAAAAY!!!!",
                         body: "Click here to restart",
                         permanent: true,
                         noPersist: true,
@@ -105,7 +105,7 @@ async function init() {
 
             if (Settings.notifyAboutUpdates)
                 setTimeout(() => showNotification({
-                    title: "A Vencord update is available!",
+                    title: "A Vencord update has been pushed, Kittycord wants to update!!!!",
                     body: "Click here to view the update",
                     permanent: true,
                     noPersist: true,
